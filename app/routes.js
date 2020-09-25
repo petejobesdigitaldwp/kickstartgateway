47c8e76c12a534d5de406ad99190ba1f46438eeb
Pete Jobes <petejobes.digitaldwp@gmail.com>
2020-09-25 13:03:58 +0100

    version 0.2 (where based)

--- app/routes.js
+++ app/routes.js
@@ -5,26 +5,3 @@ const router = express.Router()
 
 module.exports = router
 
-// Branching 0.1
-
-router.post('/version-0-1/where', function (req, res) { 
-  res.redirect('/version-0-1/lep-north-east')
-})
-
-// Branching 0.2
-
-router.post('/version-0-2/where', function (req, res) { 
-  res.redirect('/version-0-2/lep-ne')
-})
-
-// Branching 0.3
-
-router.post('/version-0-3/where', function (req, res) { 
-  res.redirect('/version-0-3/lep-ne')
-})
-
-// Branching 0.3
-
-router.post('/version-0-4/where', function (req, res) { 
-  res.redirect('/version-0-4/lep-ne')
-})
