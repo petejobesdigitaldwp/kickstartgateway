7ea534231e669dcb59b5a2f9dca485f90b12c7d9
Pete Jobes <petejobes.digitaldwp@gmail.com>
2020-10-01 18:20:12 +0100

    content updates v4

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Find a Kickstart Gateway',
+  serviceName: 'Find a Kickstart gateway',
 
   // Default port that prototype runs on
   port: '3000',
